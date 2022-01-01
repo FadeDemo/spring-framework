04a7b2dded502b7210c85bb9eb73e56bcd672ee9
paigeman <poizijiang1120@foxmail.com>
2022-01-01 20:16:37 +0800

    xml style applicationContext

--- spring-tx/src/test/java/org/fade/demo/springframework/transaction/TestCase.java
+++ spring-tx/src/test/java/org/fade/demo/springframework/transaction/TestCase.java
@@ -3,6 +3,7 @@ package org.fade.demo.springframework.transaction;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * 测试
@@ -15,7 +16,8 @@ public class TestCase {
 	@Test
 	public void main() {
 		// --add-opens java.base/java.lang=ALL-UNNAMED
-		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+//		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+		ApplicationContext context = new ClassPathXmlApplicationContext("org/fade/demo/springframework/transaction/applicationContext.xml");
 		UserService userService = context.getBean("userServiceImpl", UserService.class);
 		User user = new User();
 		user.setName("张三");
